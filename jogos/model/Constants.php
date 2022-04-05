82a5561e72ca4ec7147b92f3512d1e4d7bd74f48
Kuro020 <52428261+Kuro020@users.noreply.github.com>
2022-04-05 13:59:23 -0300

    Update Constants.php

--- jogos/model/Constants.php
+++ jogos/model/Constants.php
@@ -1,6 +1,6 @@
 <?php
 
-define('DB_HOST','jogoapi.mysql.database.azure.com');
-define('DB_USER','mateus@jogo');
+define('DB_HOST','jogos.mysql.database.azure.com');
+define('DB_USER','mateus@jogos');
 define('DB_PASS','senacSBC@123');
 define('DB_NAME','jogobd');
